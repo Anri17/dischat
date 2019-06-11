5ae162a3980b1897756313da512ea72ff0ba6b84
Andre Anri17 Almeida <andralmeida1999@gmail.com>
2019-06-11 15:06:10 +0100

    html and css done

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use('/', express.static(path.join(__dirname, 'public')));
-app.listen(PORT, () => console.log(`App started listening on port ${PORT}!`));
+app.use('/login', express.static(path.join(__dirname, 'public', 'login.html')));
+app.use('/register', express.static(path.join(__dirname, 'public', 'register.html')));
+app.use('/chat', express.static(path.join(__dirname, 'public', 'chat.html')));
 
 
+app.listen(PORT, () => console.log(`App started at on port ${PORT}!`));
